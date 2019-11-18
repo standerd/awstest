067abcfa48c871f0012ef3b04eb61f5eaa3e50e0
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2019-11-18 19:15:44 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.get("/", (req, res) => {
+  res.send("Received home request");
+});
+
+app.get("/second", (req, res) => {
+  res.send("Second File Received");
+});
+
+
+
+app.listen(process.env.PORT || 3001);
